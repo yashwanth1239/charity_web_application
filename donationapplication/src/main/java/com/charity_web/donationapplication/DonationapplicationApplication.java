07afd03fc2ee0dcddc60041f584066010d0037cb
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2025-05-29 21:23:21 +0530

    initial commit

---
+++ donationapplication/src/main/java/com/charity_web/donationapplication/DonationapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.charity_web.donationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationapplicationApplication.class, args);
+	}
+
+}
---
+++ donationapplication/src/test/java/com/charity_web/donationapplication/DonationapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charity_web.donationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
